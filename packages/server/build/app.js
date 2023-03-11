49f410270d2b492f890efb25eae768283e9457b0
Dob <lalilulelo.mg@gmail.com>
2023-03-12 08:37:23 +1100

    adding trpc

--- packages/server/build/app.js
+++ packages/server/build/app.js
@@ -1,26 +1,34 @@
-// import express from 'express'
-// import dotenv from 'dotenv'
-// const app = express()
-// if (process.env.NODE_ENV !== 'production') {
-// 	dotenv.config({ path: './.env' })
-// }
-// const PORT = process.env.PORT || 3000
-// app.get('/', (req, res) => {
-// 	res.send('Hello World! PORT' + ' ' + PORT)
-// })
-// app.listen(PORT, () => {
-// 	console.log(`Server running on  http://localhost:${PORT}`)
-// })
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-    const allUsers = await prisma.beekeeperUser.findMany();
-    console.log(allUsers);
-}
-main()
-    .catch((e) => {
-    console.error(e);
-})
-    .finally(async () => {
-    await prisma.$disconnect();
+import express from 'express';
+import dotenv from 'dotenv';
+import { initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+const createContext = ({ req, res, }) => ({}); // no context
+const t = initTRPC.context().create();
+const appRouter = t.router({
+// [...]
 });
+const app = express();
+app.use('/trpc', trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+}));
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config({ path: './.env' });
+}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on  http://localhost:${PORT}`);
+});
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
+// async function main() {
+// 	const allUsers = await prisma.beekeeperUser.findMany()
+// 	console.log(allUsers)
+// }
+// main()
+// 	.catch((e) => {
+// 		console.error(e)
+// 	})
+// 	.finally(async () => {
+// 		await prisma.$disconnect()
+// 	})
--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,36 +1,51 @@
-// import express from 'express'
-// import dotenv from 'dotenv'
+import express from 'express'
+import dotenv from 'dotenv'
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
 
-// const app = express()
+const createContext = ({
+	req,
+	res,
+  }: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+  type Context = inferAsyncReturnType<typeof createContext>;
+  const t = initTRPC.context<Context>().create();
+  const appRouter = t.router({
+	// [...]
+  });
 
-// if (process.env.NODE_ENV !== 'production') {
-// 	dotenv.config({ path: './.env' })
-// }
+const app = express()
 
-// const PORT = process.env.PORT || 3000
+app.use(
+	'/trpc',
+	trpcExpress.createExpressMiddleware({
+	  router: appRouter,
+	  createContext,
+	}),
+  );
 
-// app.get('/', (req, res) => {
-// 	res.send('Hello World! PORT' + ' ' + PORT)
-// })
-
-// app.listen(PORT, () => {
-// 	console.log(`Server running on  http://localhost:${PORT}`)
-// })
-
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-async function main() {
-	
-
-	const allUsers = await prisma.beekeeperUser.findMany()
-	console.log(allUsers) 
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config({ path: './.env' })
 }
 
-main()
-	.catch((e) => {
-		console.error(e)
-	})
-	.finally(async () => {
-		await prisma.$disconnect()
-	})
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+	console.log(`Server running on  http://localhost:${PORT}`)
+})
+
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
+
+// async function main() {
+
+// 	const allUsers = await prisma.beekeeperUser.findMany()
+// 	console.log(allUsers)
+// }
+
+// main()
+// 	.catch((e) => {
+// 		console.error(e)
+// 	})
+// 	.finally(async () => {
+// 		await prisma.$disconnect()
+// 	})
